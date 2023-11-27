7010134c346b25915a601665617a408ba371478e
Orkhan647 <okuw.k@mail.ru>
2023-11-27 13:22:19 +0400

    fix navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,28 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import AppBar from '@mui/material/AppBar';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import { Container, Button, Box, Drawer, Divider } from '@mui/material';
-import logo from '../../assets/logo/logo.png';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Link as RouterLink, useLocation, useNavigate } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import { Container, Button, Box, Drawer, Divider, Link } from "@mui/material";
+import logo from "../../assets/logo/logo.png";
 
 const navItems = [
-  { label: 'Home', path: '/' },
-  { label: 'Blog', path: '/blog' },
-  { label: 'About', path: '/about' },
-  { label: 'Contact', path: '/contact' },
+  { label: "Home", path: "/" },
+  { label: "Blog", path: "/blog" },
+  { label: "About", path: "/about" },
+  { label: "Contact", path: "/contact" },
 ];
 
 const Navbar = (props) => {
+  const handleScroll = () => {
+    window.scrollTo(0, 0);
+  };
   const { window } = props;
   const [openDrawer, setOpenDrawer] = useState(false);
   const location = useLocation();
@@ -30,14 +33,14 @@ const Navbar = (props) => {
   };
 
   const isItemSelected = (item) => {
-    return location.pathname === (item.path === '/' ? '/' : `${item.path}`);
+    return location.pathname === (item.path === "/" ? "/" : `${item.path}`);
   };
 
   const handleClick = (item) => {
     setOpenDrawer(false);
 
-    if (item.label === 'Subscribe') {
-      console.log('Subscribe button clicked');
+    if (item.label === "Subscribe") {
+      console.log("Subscribe button clicked");
     }
   };
 
@@ -49,21 +52,22 @@ const Navbar = (props) => {
             <IconButton
               color="inherit"
               edge="start"
-              sx={{ mr: 2, display: { sm: 'none' } }}
+              sx={{ mr: 2, display: { sm: "none" } }}
               onClick={() => setOpenDrawer(true)}
             >
               <MenuIcon />
             </IconButton>
-            <Box sx={{ flexGrow: 1, cursor: 'pointer' }}>
+            <Box sx={{ flexGrow: 1, cursor: "pointer" }}>
               <img src={logo} alt="" width={250} height={50} />
             </Box>
-            <List sx={{ display: { xs: 'none', gap: '10px', sm: 'flex' } }}>
+            <List sx={{ display: { xs: "none", gap: "10px", sm: "flex" } }}>
               {navItems.map((item) => (
                 <ListItem disablePadding key={item.label}>
-                  <Link
+                  <RouterLink
                     to={item.path}
                     underline="none"
                     color="inherit"
+                    style={{ color: "white", textDecoration: "none" }}
                   >
                     <ListItemButton
                       selected={isItemSelected(item)}
@@ -80,29 +84,24 @@ const Navbar = (props) => {
                     >
                       <ListItemText primary={item.label} />
                     </ListItemButton>
-                  </Link>
+                  </RouterLink>
                 </ListItem>
               ))}
               <ListItem>
-                <Link
-                  to="/subscribe"
+                <Button
                   underline="none"
-                  color="inherit"
                   sx={{
+                    color: "black",
                     bgcolor: (theme) => theme.palette.secondary.main,
-                    marginLeft: '50px',
-                    '&:hover': {
-                      bgcolor: 'white',
+                    marginLeft: "50px",
+                    "&:hover": {
+                      bgcolor: "white",
+                      color: "black",
                     },
-                  }}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    handleClick({ label: 'Subscribe' });
-                    navigate('/subscribe');
                   }}
                 >
                   <ListItemText primary="Subscribe" />
-                </Link>
+                </Button>
               </ListItem>
             </List>
           </Toolbar>
@@ -113,9 +112,10 @@ const Navbar = (props) => {
         open={openDrawer}
         onClose={() => setOpenDrawer(false)}
       >
-        <List sx={{ margin: '0 auto', width: '100%' }}>
+        <List sx={{ margin: "0 auto", width: "100%" }}>
           {navItems.map((item) => (
             <ListItem
+              component={RouterLink}
               to={item.path}
               key={item.label}
               onClick={() => {
@@ -124,8 +124,8 @@ const Navbar = (props) => {
                 navigate(item.path);
               }}
               sx={{
-                textAlign: 'center',
-                '&:hover': {
+                textAlign: "center",
+                "&:hover": {
                   backgroundColor: (theme) => theme.palette.secondary.main,
                 },
               }}
