400ab95d348b372304c52bd095fcfd7f6522325a
Orkhan647 <okuw.k@mail.ru>
2023-12-06 13:15:06 +0400

    test

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import PageContainer from "../../components/PageContainer/PageContainer";
 import AboutPageHead from "../../components/AboutPageHead/AboutPageHead";
 import AboutOurTeam from "../../components/AboutOurTeam/AboutOurTeam";
