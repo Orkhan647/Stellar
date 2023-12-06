c0ae8f21a4829f097f805bd65d0d61e0596594a1
Orkhan647 <okuw.k@mail.ru>
2023-12-06 13:17:33 +0400

    fix

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PageContainer from "../../components/PageContainer/PageContainer";
 import AboutPageHead from "../../components/AboutPageHead/AboutPageHead";
 import AboutOurTeam from "../../components/AboutOurTeam/AboutOurTeam";
@@ -6,9 +6,9 @@ import AboutAuthors from "../../components/AboutListAuthors/AboutAuthors";
 import JoinOurTeam from "../../components/JoinOurTeam/JoinOurTeam";
 
 const About = () => {
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <PageContainer>
       <AboutPageHead/>
